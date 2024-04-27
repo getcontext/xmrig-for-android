cd60fa3ebdbb99d9518ee4acf5148f80c0b362d9
getcontext <andrzej.salamon@gmail.com>
2024-04-27 16:16:42 +0200

    fetch xmrig

--- xmrig/lib-builder/script/xmrig-fetch.sh
+++ xmrig/lib-builder/script/xmrig-fetch.sh
@@ -6,18 +6,16 @@ source script/env.sh
 
 cd $EXTERNAL_LIBS_BUILD_ROOT
 
-version="v6.17.0"
+version="master"
 
 if [ ! -d "xmrig" ]; then
-  git clone https://github.com/xmrig/xmrig.git -b ${version}
+  git clone https://github.com/getcontext/xmrig -b ${version}
   cd ..
   cd ..
-  patch build/src/xmrig/src/net/strategies/DonateStrategy.cpp ./xmrig.patch --force
 else
   cd xmrig
   git checkout ${version}
   cd ..
   cd ..
   cd ..
-  patch build/src/xmrig/src/net/strategies/DonateStrategy.cpp ./xmrig.patch --force
 fi
